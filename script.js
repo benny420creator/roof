23f7a2ced5a33f8dd36b02d47216d3370d9f541b
benny420creator <118639969+benny420creator@users.noreply.github.com>
2023-12-11 14:07:05 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function calculate() {
     let desiredRValue = parseFloat(document.getElementById("desired_r_value").value);
-    let resultText = "";
+    let resultsTable = document.getElementById("resultsTable");
+
+    // Clear previous results
+    resultsTable.innerHTML = "<tr><th>Area</th><th>Required Thickness (inches)</th></tr>";
 
     // Area 1 Calculation
     let rValuePerInch1 = parseFloat(document.getElementById("material1").value);
     let area1 = parseFloat(document.getElementById("area1_value").value);
     if (!isNaN(rValuePerInch1) && !isNaN(area1) && area1 > 0) {
         let requiredThickness1 = desiredRValue / rValuePerInch1;
-        resultText += `Area 1 requires ${requiredThickness1.toFixed(2)} inches of selected material<br>`;
+        resultsTable.innerHTML += `<tr><td>Area 1</td><td>${requiredThickness1.toFixed(2)}</td></tr>`;
     }
 
     // Area 2 Calculation with Custom R-Value
@@ -15,8 +18,6 @@ function calculate() {
     let customRValue = parseFloat(document.getElementById("area2_rvalue").value);
     if (!isNaN(customRValue) && !isNaN(area2) && area2 > 0) {
         let requiredThickness2 = desiredRValue / customRValue;
-        resultText += `Area 2 requires ${requiredThickness2.toFixed(2)} inches of custom material<br>`;
+        resultsTable.innerHTML += `<tr><td>Area 2</td><td>${requiredThickness2.toFixed(2)}</td></tr>`;
     }
-
-    document.getElementById("result").innerHTML = resultText;
 }
