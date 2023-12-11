45641ed52f8a49e2e595e494e1abce890d88495d
benny420creator <118639969+benny420creator@users.noreply.github.com>
2023-12-11 14:13:04 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 function calculate() {
     let desiredRValue = parseFloat(document.getElementById("desired_r_value").value);
-    let resultsTable = document.getElementById("resultsTable");
-
-    // Clear previous results
-    resultsTable.innerHTML = "<tr><th>Area</th><th>Required Thickness (inches)</th></tr>";
 
     // Area 1 Calculation
     let rValuePerInch1 = parseFloat(document.getElementById("material1").value);
     let area1 = parseFloat(document.getElementById("area1_value").value);
-    if (!isNaN(rValuePerInch1) && !isNaN(area1) && area1 > 0) {
-        let requiredThickness1 = desiredRValue / rValuePerInch1;
-        resultsTable.innerHTML += `<tr><td>Area 1</td><td>${requiredThickness1.toFixed(2)}</td></tr>`;
-    }
+    let requiredThickness1 = !isNaN(rValuePerInch1) && area1 > 0 ? desiredRValue / rValuePerInch1 : 0;
 
     // Area 2 Calculation with Custom R-Value
     let area2 = parseFloat(document.getElementById("area2_value").value);
     let customRValue = parseFloat(document.getElementById("area2_rvalue").value);
-    if (!isNaN(customRValue) && !isNaN(area2) && area2 > 0) {
-        let requiredThickness2 = desiredRValue / customRValue;
-        resultsTable.innerHTML += `<tr><td>Area 2</td><td>${requiredThickness2.toFixed(2)}</td></tr>`;
-    }
+    let requiredThickness2 = !isNaN(customRValue) && area2 > 0 ? desiredRValue / customRValue : 0;
+
+    // Calculating average thickness
+    let totalArea = area1 + area2;
+    let averageThickness = (requiredThickness1 * area1 + requiredThickness2 * area2) / totalArea;
+    averageThickness = isNaN(averageThickness) ? 0 : averageThickness;
+
+    let resultText = `Average thickness required for both areas: ${averageThickness.toFixed(2)} inches`;
+
+    document.getElementById("result").innerHTML = resultText;
 }
