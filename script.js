2396c3005fa6b88059b257d45354cc28f00c8ccf
benny420creator <118639969+benny420creator@users.noreply.github.com>
2023-12-11 13:50:52 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-function calculateRValue() {
-    var area1 = document.getElementById('area1').value;
-    var rValue1 = document.getElementById('rValue1').value;
-    var area2 = document.getElementById('area2').value;
-    var overallUValue = document.getElementById('overallUValue').value;
+function calculate() {
+    let totalRValue = 0;
+    let totalArea = 0;
+    let desiredRValue = parseFloat(document.getElementById("desired_r_value").value);
 
-    var totalArea = parseFloat(area1) + parseFloat(area2);
-    var totalRValue = 1 / overallUValue;
-    var requiredRValue2 = (totalRValue * totalArea - rValue1 * area1) / area2;
+    // Area 1 Calculation
+    let rValuePerInch1 = parseFloat(document.getElementById("material1").value);
+    let area1 = parseFloat(document.getElementById("area1_value").value);
+    if (!isNaN(rValuePerInch1) && !isNaN(area1) && area1 > 0) {
+        totalRValue += rValuePerInch1 * area1;
+        totalArea += area1;
+    }
 
-    document.getElementById('output').textContent = requiredRValue2.toFixed(2);
+    // Area 2 Calculation with Custom R-Value
+    let area2 = parseFloat(document.getElementById("area2_value").value);
+    let customRValue = parseFloat(document.getElementById("area2_rvalue").value);
+    if (!isNaN(customRValue) && !isNaN(area2) && area2 > 0) {
+        totalRValue += customRValue * area2;
+        totalArea += area2;
+    }
+
+    let effectiveRValue = totalArea > 0 ? totalRValue / totalArea : 0;
+    document.getElementById("result").innerHTML = `Effective R-Value: ${effectiveRValue.toFixed(2)}`;
 }
