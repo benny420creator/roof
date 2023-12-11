7fe542be27cbd952fc3ca9cbefe3f6920fc147ec
benny420creator <118639969+benny420creator@users.noreply.github.com>
2023-12-11 12:50:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function calculateRValue() {
     var area1 = document.getElementById('area1').value;
     var rValue1 = document.getElementById('rValue1').value;
     var area2 = document.getElementById('area2').value;
-    var overallUValue = 0.063;
+    var overallUValue = document.getElementById('overallUValue').value;
 
     var totalArea = parseFloat(area1) + parseFloat(area2);
     var totalRValue = 1 / overallUValue;
